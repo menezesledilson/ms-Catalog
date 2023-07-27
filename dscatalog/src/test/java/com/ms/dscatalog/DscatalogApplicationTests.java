d024e28e00e4fa5b8b0b6c3660b3fc8075e3fb54
menezesledilson <ledilsonmenezes@gmail.com>
2023-07-27 16:26:51 -0300

    Create project

---
+++ dscatalog/src/test/java/com/ms/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

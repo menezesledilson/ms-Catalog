31a513706d99b99aaeafba07e06af925681c1a00
menezesledilson <ledilsonmenezes@gmail.com>
2023-07-31 19:29:47 -0300

    DTO

--- dscatalog/src/main/java/com/ms/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/ms/dscatalog/services/CategoryService.java
@@ -1,10 +1,13 @@
 package com.ms.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ms.dscatalog.dto.CategoryDTO;
 import com.ms.dscatalog.entities.Category;
 import com.ms.dscatalog.repositories.CategoryRepository;
 
@@ -14,9 +17,16 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List <Category> list = repository.findAll();
 		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+// 		List<CategoryDTO> listDTO = new ArrayList<>();
+// 		for (Category  cat: list) {
+// 			listDTO.add(new CategoryDTO(cat));
+// 	}
+// 		return listDTO;
 	}
 
 }

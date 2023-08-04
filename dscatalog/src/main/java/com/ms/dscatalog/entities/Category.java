a5d7b9f1f69401dff6a417995f1ac1ff8c83495d
menezesledilson <ledilsonmenezes@gmail.com>
2023-08-04 14:40:30 -0300

    Auditing data

--- dscatalog/src/main/java/com/ms/dscatalog/entities/Category.java
+++ dscatalog/src/main/java/com/ms/dscatalog/entities/Category.java
@@ -4,11 +4,15 @@
 package com.ms.dscatalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 /**
@@ -23,13 +27,18 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
+
 	public Category() {
-		
+
 	}
 
 	public Category(Long id, String name) {
-		 
+
 		this.id = id;
 		this.name = name;
 	}
@@ -48,6 +57,34 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	
+
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+//	public void setCreatedAt(Instant createdAt) {
+//		this.createdAt = createdAt;
+//	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+//	public void setUpdatedAt(Instant updatedAt) {
+//		this.updatedAt = updatedAt;
+//	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
 	}
 
 	@Override
@@ -75,7 +112,4 @@ public class Category implements Serializable {
 		return true;
 	}
 
-
-	
-	
 }

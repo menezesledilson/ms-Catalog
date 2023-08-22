e0e9bdc263535fa96733d58ea36625f10506623d
menezesledilson <ledilsonmenezes@gmail.com>
2023-08-22 20:54:55 -0300

    Refactor pageable

--- dscatalog/src/main/java/com/ms/dscatalog/resources/CategoryResource.java
+++ dscatalog/src/main/java/com/ms/dscatalog/resources/CategoryResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -30,18 +31,9 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<Page<CategoryDTO>> findAll(
-			
-			@RequestParam( value ="page", defaultValue = "0")Integer page,
-			@RequestParam( value ="linesPerPage", defaultValue = "12")Integer linesPerPage,
-			@RequestParam( value ="direction", defaultValue = "ASC")String direction,
-			@RequestParam( value ="ordeyBy", defaultValue = "name")String orderBy
-			){
-		
-		PageRequest pageRequest = PageRequest.of(page,linesPerPage,Direction.valueOf(direction),orderBy );
-		
-		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
-		
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) {
+
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 
@@ -60,10 +52,11 @@ public class CategoryResource {
 	}
 
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<CategoryDTO> update(@PathVariable Long id,@RequestBody CategoryDTO dto) {
+	public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO dto) {
 		dto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
+
 	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<CategoryDTO> delete(@PathVariable Long id) {
 		service.delete(id);
